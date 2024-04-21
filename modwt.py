3c10b4ca3bdbf00474ab17e84df754af20fcb0aa
Keshav Gupta <102588113+keshav0306@users.noreply.github.com>
2024-04-21 19:21:40 +0530

    Update modwt.py
    modified modwt.py

--- modwt.py
+++ modwt.py
@@ -3,40 +3,6 @@ import pdb
 import pywt
 from scipy.ndimage import convolve1d
 
-
-def upArrow_op(li, j):
-    if j == 0:
-        return [1]
-    N = len(li)
-    li_n = np.zeros(2**(j - 1) * (N - 1) + 1)
-    for i in range(N):
-        li_n[2**(j - 1) * i] = li[i]
-    return li_n
-
-
-def period_list(li, N):
-    n = len(li)
-    # append [0 0 ...]
-    n_app = N - np.mod(n, N)
-    li = list(li)
-    li = li + [0] * n_app
-    if len(li) < 2 * N:
-        return np.array(li)
-    else:
-        li = np.array(li)
-        li = np.reshape(li, [-1, N])
-        li = np.sum(li, axis=0)
-        return li
-
-
-def circular_convolve_mra(h_j_o, w_j):
-    ''' calculate the mra D_j'''
-    return convolve1d(w_j,
-                      np.flip(h_j_o),
-                      mode="wrap",
-                      origin=(len(h_j_o) - 1) // 2)
-
-
 def circular_convolve_d(h_t, v_j_1, j):
     '''
     jth level decomposition
@@ -52,8 +18,7 @@ def circular_convolve_d(h_t, v_j_1, j):
     for i, h in enumerate(h_t):
         ker[i * 2**(j - 1)] = h
     
-
-    w_j = convolve1d(v_j_1, ker, mode="wrap", origin=-len(ker) // 2)
+    w_j = convolve1d(v_j_1, ker, mode="wrap", origin=-(len(ker) // 2))
     return w_j
 
 
@@ -105,7 +70,6 @@ def modwt(x, filters, level):
     wavecoeff.append(v_j_1)
     return np.vstack(wavecoeff)
 
-
 def imodwt(w, filters):
     ''' inverse modwt '''
     # filter
@@ -121,44 +85,7 @@ def imodwt(w, filters):
         v_j = circular_convolve_s(h_t, g_t, w[j], v_j, j + 1)
     return v_j
 
-
-def modwtmra(w, filters):
-    ''' Multiresolution analysis based on MODWT'''
-    # filter
-    wavelet = pywt.Wavelet(filters)
-    h = wavelet.dec_hi
-    g = wavelet.dec_lo
-    # D
-    level, N = w.shape
-    level = level - 1
-    D = []
-    g_j_part = [1]
-    for j in range(level):
-        # g_j_part
-        g_j_up = upArrow_op(g, j)
-        g_j_part = np.convolve(g_j_part, g_j_up)
-        # h_j_o
-        h_j_up = upArrow_op(h, j + 1)
-        h_j = np.convolve(g_j_part, h_j_up)
-        h_j_t = h_j / (2**((j + 1) / 2.))
-        if j == 0: h_j_t = h / np.sqrt(2)
-        h_j_t_o = period_list(h_j_t, N)
-        D.append(circular_convolve_mra(h_j_t_o, w[j]))
-    # S
-    j = level - 1
-    g_j_up = upArrow_op(g, j + 1)
-    g_j = np.convolve(g_j_part, g_j_up)
-    g_j_t = g_j / (2**((j + 1) / 2.))
-    g_j_t_o = period_list(g_j_t, N)
-    S = circular_convolve_mra(g_j_t_o, w[-1])
-    D.append(S)
-    return np.vstack(D)
-
-
 if __name__ == '__main__':
-    s1 = np.arange(10)
+    s1 = np.arange(10).astype(np.float32)
     ws = modwt(s1, 'db2', 3)
     s1p = imodwt(ws, 'db2')
-    mra = modwtmra(ws, 'db2')
-    print(ws)
-    print(mra)
